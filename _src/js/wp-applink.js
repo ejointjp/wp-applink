6ea7980e5f164e6dbe6873dc902ce4b0e3bf9755
Takashi Fujisaki <mail@e-joint.jp>
2017-04-28 11:18:18 +0900

    fix js カテゴリ選択が無効になっていた

--- _src/js/wp-applink.js
+++ _src/js/wp-applink.js
@@ -1,28 +1,30 @@
 (function($){
 
   $(function(){
-  	var _locale_ = document.getElementsByTagName('html')[0].getAttribute('lang');
-  	var $status = $('#wpal-status');
-  	var $select = $('#wpal-select');
-  	var $media = $('#wpal-media');
-  	var $entity = $('#wpal-entity');
-  	var $screenshot = $('#wpal-screenshot');
-  	var $limit = $('#wpal-limit');
-  	var $term = $('#wpal-term');
-  	var $result = $('#wpal-result');
-  	var $loader = $('#wpal-loader');
-  	var $submit = $('#wpal-submit');
+    var _locale_ = document.getElementsByTagName('html')[0].getAttribute('lang');
+    var $status = $('#wpal-status');
+    var $select = $('#wpal-select');
+    var $media = $('#wpal-media');
+    var $entity = $('#wpal-entity');
+    var $screenshot = $('#wpal-screenshot');
+    var $limit = $('#wpal-limit');
+    var $term = $('#wpal-term');
+    var $result = $('#wpal-result');
+    var $loader = $('#wpal-loader');
+    var $submit = $('#wpal-submit');
 
-  	var term = $term.val();
-  	var jqxhr;
-    var timer = false;
-    var suspension = 1000; // keyupイベント発動の猶予時間（s）
+    var term = $term.val();
+    var jqxhr;
 
-  	set_hidden_val();
+    set_hidden_val();
 
-  	$submit.on('click', function(){
-  		ajax_send();
-  	});
+    $submit.on('click', function(){
+      ajax_send();
+    });
+
+    $select.on('change', function(){
+      set_hidden_val();
+    });
 
     $term.on('keydown', function(e){
       // 検索ワード入力時にEnterで検索を実行。
@@ -36,67 +38,67 @@
     });
 
 
-  	function ajax_send(){
+    function ajax_send(){
 
-  		term = $term.val();
+      term = $term.val();
 
-  		if (jqxhr) {
-  			jqxhr.abort(); //ajax()実行前にjqxhrオブジェクトを判定し、オブジェクトが存在すればabort()を実行
-  		}
+      if (jqxhr) {
+        jqxhr.abort(); //ajax()実行前にjqxhrオブジェクトを判定し、オブジェクトが存在すればabort()を実行
+      }
 
-  		if(term === ''){
-  			$loader.slideUp(100);
-  			if(_locale_ === 'en' || _locale_ ==='en_US'){
-  				$result.text('Please Enter search keywords.');
-  			} else {
-  				$result.text('検索キーワードを入力してください');
-  			}
-  			$term.focus();
-  		}
+      if(term === ''){
+        $loader.slideUp(100);
+        if(_locale_ === 'en' || _locale_ ==='en_US'){
+          $result.text('Please Enter search keywords.');
+        } else {
+          $result.text('検索キーワードを入力してください');
+        }
+        $term.focus();
+      }
 
-  		else{
+      else{
 
-  			$result.text('');
-  			$loader.slideDown(100);
+        $result.text('');
+        $loader.slideDown(100);
 
-  			jqxhr = $.ajax({
-  				url: ajaxurl,
-  				// data: $form.serialize(),
-  				data: {
-  					media: $media.val(),
-  					entity: $entity.val(),
-  					screenshot: $screenshot.val(),
-  					limit: $limit.val(),
-  					term: $term.val(),
-  					at: $('#wpal-token').val(),
-  					action: 'wpal_ajax_search'
-  				},
-  				type: 'GET'
-  			})
+        jqxhr = $.ajax({
+          url: ajaxurl,
+          // data: $form.serialize(),
+          data: {
+            media: $media.val(),
+            entity: $entity.val(),
+            screenshot: $screenshot.val(),
+            limit: $limit.val(),
+            term: $term.val(),
+            at: $('#wpal-token').val(),
+            action: 'wpal_ajax_search'
+          },
+          type: 'GET'
+        })
 
-  			.done(function(data){
-  				$result.html(data);
+        .done(function(data){
+          $result.html(data);
           $loader.slideUp(100);
-  			})
+        })
 
-  			.fail(function(jqXHR, textStatus){
-  				if(textStatus !== 'abort'){
-  					if(_locale_ === 'en' || _locale_ ==='en_US'){
-  						$status.text('Connection failed.');
-  					} else {
-  						$status.text('接続に失敗しました。');
-  					}
-  					$loader.slideUp(100);
-  				}
-  			});
-  		}
+        .fail(function(jqXHR, textStatus){
+          if(textStatus !== 'abort'){
+            if(_locale_ === 'en' || _locale_ ==='en_US'){
+              $status.text('Connection failed.');
+            } else {
+              $status.text('接続に失敗しました。');
+            }
+            $loader.slideUp(100);
+          }
+        });
+      }
       console.log('発動！');
-  	}
+    }
 
 
-  	function set_hidden_val(){
+    function set_hidden_val(){
 
-  		selectVal = $select.val();
+      var selectVal = $select.val();
 
       if(selectVal == 'iPadSoftware' || selectVal == 'macSoftware'){
         $media.val('software');
@@ -110,7 +112,7 @@
         $media.val(selectVal);
         $entity.val('');
       }
-  	}
+    }
   });
 
 })(jQuery);
